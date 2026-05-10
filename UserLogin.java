d297be70467d1b07416d4128ced41b17221a6a04
jaya <jayasreebanda510@gmail.com>
2026-05-10 16:56:35 +0530

    Implemented banking account types, validation checks, and interest handling

--- UserLogin.java
+++ UserLogin.java
@@ -2,7 +2,7 @@ package BankingApplication;
 
 import java.util.Scanner;
 
-public abstract class UserLogin {
+public abstract class UserLogin extends AccountTypeImplementation{
 	static public Scanner sc = new Scanner(System.in);
 	private String accountHolderName;
 	private long mobileNumber;
@@ -13,17 +13,16 @@ public abstract class UserLogin {
 	private String fatherOrHusbandOF;
 	private double initialAmountDeposit;
 	private String martialStatus;
-	
 
 	public UserLogin(String accountHolderName, long mobileNumber, long adharCardNumber, String address,
 			String fatherOrHusbandOF) {
 
-		this(accountHolderName, adharCardNumber, "savingsBank", 0.0, address, fatherOrHusbandOF, mobileNumber,
+		this(accountHolderName, adharCardNumber, "savings", 0.0, address, fatherOrHusbandOF, mobileNumber,
 				"single");
 	}
 
 	public UserLogin(String accountHolderName, long adharCardNumber, String bankType, double initialAmountDeposit,
-			String address, String fatherOrHusbandOF, long  mobileNumber, String martialStatus) {
+			String address, String fatherOrHusbandOF, long mobileNumber, String martialStatus) {
 		this(accountHolderName, adharCardNumber, bankType, initialAmountDeposit, null, address, fatherOrHusbandOF,
 				mobileNumber, martialStatus);
 
@@ -31,12 +30,12 @@ public abstract class UserLogin {
 
 	public UserLogin(String accountHolderName, long adharCardNumber, String bankType, double initialAmountDeposit,
 			String pANCardNumber, String address, String fatherOrHusbandOF, long mobileNumber, String martialStatus) {
-		super();
+		super(initialAmountDeposit,bankType);
 		this.accountHolderName = accountHolderName;
-		this.mobileNumber = mobileNumber;
-		this.adharCardNumber = adharCardNumber;
+		this.mobileNumber = validationCheckingMobile(mobileNumber);
+		this.adharCardNumber = validationCheckingAdhar(adharCardNumber);
 		this.bankType = bankType;
-		this.PANCardNumber = pANCardNumber;
+		this.PANCardNumber = validationCheckingPAN(pANCardNumber);
 		this.address = address;
 		this.fatherOrHusbandOF = fatherOrHusbandOF;
 		this.initialAmountDeposit = initialAmountDeposit;
@@ -50,8 +49,34 @@ public abstract class UserLogin {
 		System.out.println("Application Form");
 		System.out.println("Make sure to fill All the Details");
 		System.out.println("enter Account Holder Name");
-		
 
+	}
+
+	 long validationCheckingMobile(long mobileNumber) {
+		if (String.valueOf(mobileNumber).matches("[0-9]{10}")) {
+			return mobileNumber;
+		} else {
+
+			throw new IllegalArgumentException("Invalid Mobile Number");
+		}
+	}
+
+	long validationCheckingAdhar(long adharCardNumber) {
+		if (String.valueOf(adharCardNumber).matches("[0-9]{12}")) {
+			return adharCardNumber;
+
+		} else {
+			throw new IllegalArgumentException("Invalid PAN Card Number");
+		}
+	}
+
+	String validationCheckingPAN(String PANCardNumber) {
+		if (PANCardNumber.matches("[A-Z]{5}[0-9]{4}[A-Z]{1}")) {
+			return PANCardNumber;
+		}
+		else {
+			throw new IllegalArgumentException("Invalid PAN Card Number");
+		}
 	}
 
 }
